f6d38e666dc7629c14f6d90625c5061ceeee1c7f
Pahri <ftyvfyy@gmail.com>
2025-12-19 09:46:32 +0700

    footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-neutral-primary-soft rounded-base shadow-xs border border-default w-full fixed bottom-0 left-0 z-10">
-    <div class="w-full mx-auto max-w-screen-xl p-4 flex justify-center">
+    <div class="w-full mx-auto max-w-7xl p-4 flex justify-center">
         <span class="text-sm text-body text-center">
             © 2025 <a href="" class="hover:underline">Archie</a>. All Rights Reserved.
         </span>
